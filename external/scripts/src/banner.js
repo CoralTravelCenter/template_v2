ce84b39de36d41cd592e2e62991b10f3b904d306
alex_shkardun <alexander.shkardun@coral.ru>
2025-07-14 17:55:27 +0300

    #coral - баннер бизнес

--- external/scripts/src/banner.js
+++ external/scripts/src/banner.js
@@ -1,27 +1,59 @@
-const container = document.querySelector('[data-testid="test-package-tour-flight-widget"]');
+// const container = document.querySelector('[data-testid="test-package-tour-flight-widget"]');
+// const heading = document.querySelector('.countdown-timer');
+//
+// console.log(heading);
+//
+// if (container) {
+//     const observer = new MutationObserver(mutations => {
+//         if (window.innerWidth > 768) {
+//             console.log(1)
+//
+//         } else {
+//             console.log(2)
+//
+//         }
+//     });
+//
+//     observer.observe(document.body, {
+//         childList: true,
+//         subtree: true,
+//     });
+// }
 
-if (container) {
-    const observer = new MutationObserver(mutations => {
-        if (window.innerWidth > 768) {
-            const header = document.querySelector('.header-wrapper');
-            const divider = header?.nextElementSibling;
+setTimeout(() => {
+    if (window.innerWidth <= 768) {
+        const mobileLocation = document.querySelector('[aria-describedby="«ri»"]');
 
-            if (divider && divider.classList.contains('ant-divider')) {
-                divider.insertAdjacentHTML('beforeend', '<div><img style="border-radius: 12px; width: 100%;" src="https://b2ccdn.coral.ru/content/mindbox/business-flight/business-flight.webp" alt=""></div>');
-                observer.disconnect();
-            }
-        } else {
-            const header = document.getElementById('package-tour-flight-hotel-overview-heading-area');
+        if (mobileLocation) {
+            const locationText = mobileLocation.textContent.trim();
+            const cityCode = locationText.split('-')[0].trim().toUpperCase();
 
-            if (header) {
-                header.insertAdjacentHTML('afterend', '<div><img style="border-radius: 12px; width: 100%;" src="https://b2ccdn.coral.ru/content/mindbox/business-flight/business-flight_m.webp" alt=""></div>');
-                observer.disconnect();
+            if (cityCode === 'AYT') {
+                const header = document.getElementById('package-tour-flight-hotel-overview-heading-area');
+
+                if (header) {
+                    header.insertAdjacentHTML('afterend', '<div><img style="border-radius: 12px; width: 100%;" src="https://b2ccdn.coral.ru/content/mindbox/business-flight/bs-nord_m.webp" alt=""></div>');
+                }
             }
         }
-    });
+    } else {
+        const card = document.getElementById('package-tour-flight-hotel-overview-flight-information');
 
-    observer.observe(container, {
-        childList: true,
-        subtree: true,
-    });
-}
+        if (card) {
+            const locations = card.querySelectorAll('.flight-location');
+            const locationText = locations[1].textContent.trim();
+            const cityCode = locationText.split('-')[0].trim().toUpperCase();
+
+            if (cityCode === 'AYT') {
+                const header = document.querySelector('.header-wrapper');
+                const divider = header?.nextElementSibling;
+
+                if (divider && divider.classList.contains('ant-divider')) {
+                    divider.insertAdjacentHTML('beforeend', '<div><img style="border-radius: 12px; width: 100%;" src="https://b2ccdn.coral.ru/content/mindbox/business-flight/bs-nord.webp" alt=""></div>');
+                }
+            }
+        }
+    }
+}, 2000);
+
+
