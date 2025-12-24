0373d4de5af526c7f8a39e3323ae116c054e7d19
alex_shkardun <alexander.shkardun@coral.ru>
2025-12-24 15:36:26 +0300

    метрика для логотипа

--- external/scripts/vite.config.js
+++ external/scripts/vite.config.js
@@ -4,11 +4,11 @@ import monkey from 'vite-plugin-monkey';
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/newStub.js',
+      entry: 'src/logoMetric.js',
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.coral.ru/preview/4a18f887-3ad5-465c-b21c-c40c16a8ddce/ru-RU/'],
+        match: ['https://www.coral.ru/'],
       },
     }),
   ],
