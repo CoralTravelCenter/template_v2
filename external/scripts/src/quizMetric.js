5a810057eea2b3ccc6734a8c471114cb3bda9b2f
Alex Shkardun <slipnir@yandex.ru>
2026-02-12 10:04:56 +0300

    метрика для квиза

--- external/scripts/src/quizMetric.js
+++ external/scripts/src/quizMetric.js
@@ -6,63 +6,34 @@
             obs.disconnect();
             const shadowRoot = quizContainer.shadowRoot;
 
+            shadowRoot.addEventListener('click', (e) => {
+                const link = e.target.closest('a');
+
+                if (!link) return;
+
+                switch(link.title) {
+                    case 'Ночная жизнь в Таиланде':
+                        ym(96674199, 'reachGoal', '14_02_quiz_finish', {'product': 'Таиланд'});
+                        break;
+                    case 'Яркие впечатления в ОАЭ':
+                        ym(96674199, 'reachGoal', '14_02_quiz_finish', {'product': 'ОАЭ'});
+                        break;
+                    case 'Беззаботный отдых в Египте':
+                        ym(96674199, 'reachGoal', '14_02_quiz_finish', {'product': 'Египет'});
+                        break;
+                    case 'Уединение на Мальдивах':
+                        ym(96674199, 'reachGoal', '14_02_quiz_finish', {'product': 'Мальдивы'});
+                        break;
+                }
+            });
+
             const startButton = shadowRoot.querySelector('.go2693040066');
 
-            startButton.addEventListener('click', (e) => {
-                ym(96674199,'reachGoal', '14_02_quiz_start');
-
-                const innerObs = new MutationObserver(() => {
-                    const enterButton = shadowRoot.querySelector('.go3912719842');
-
-                    if (enterButton) {
-                        // innerObs.disconnect();
-
-                        const links = shadowRoot.querySelectorAll('a');
-                        const buttons = shadowRoot.querySelectorAll('button');
-
-                        if (links.length > 0) {
-                            innerObs.disconnect();
-
-                            links.forEach(link => {
-                                if (link.title === 'Ночная жизнь в Таиланде') {
-                                    link.addEventListener('click', () => {
-                                        ym(96674199,'reachGoal', '14_02_quiz_finish', {'product': 'Таиланд'});
-                                    });
-                                }
-
-                                if (link.title === 'Яркие впечатления в ОАЭ') {
-                                    link.addEventListener('click', () => {
-                                        ym(96674199,'reachGoal', '14_02_quiz_finish', {'product': 'ОАЭ'});
-                                    });
-                                }
-
-                                if (link.title === 'Беззаботный отдых в Египте') {
-                                    link.addEventListener('click', () => {
-                                        ym(96674199,'reachGoal', '14_02_quiz_finish', {'product': 'Египет'});
-                                    });
-                                }
-
-                                if (link.title === 'Уединение на Мальдивах') {
-                                    link.addEventListener('click', () => {
-                                        ym(96674199,'reachGoal', '14_02_quiz_finish', {'product': 'Мальдивы'});
-                                    });
-                                }
-                            });
-                        }
-
-                        buttons.forEach(button => {
-                            button.addEventListener('click', () => {
-
-                            });
-                        });
-                    }
-                });
-
-                innerObs.observe(shadowRoot, {
-                    childList: true,
-                    subtree: true,
-                });
-            });
+            if (startButton) {
+                startButton.addEventListener('click', (e) => {
+                    ym(96674199, 'reachGoal', '14_02_quiz_start');
+                }, { once: true });
+            }
         }
     });
 
