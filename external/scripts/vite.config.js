ba5241e146b4920baa6ca9b716ebfbf9802099ed
Alex Shkardun <slipnir@yandex.ru>
2026-04-23 17:54:18 +0300

    баннер для onlyflight

--- external/scripts/vite.config.js
+++ external/scripts/vite.config.js
@@ -4,14 +4,14 @@ import monkey from 'vite-plugin-monkey';
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/miniPage.js',
+      entry: 'src/onlyflightBanner.js',
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
         match: [
           // 'https://www.coral.ru/hotels/turkey/*',
           // 'https://www.coral.ru/booking/add-passenger*'
-            'https://www.sunmar.ru/preview/9db04aca-cd69-47cb-9610-f45d7e4ec4f3/ru-RU/'
+            'https://www.coral.ru/onlyflight/*'
         ],
       },
     }),
