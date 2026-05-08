8b035dcf89db360887dca71e5fa0608c0219df6d
alex_shkardun <alexander.shkardun@coral.ru>
2026-05-08 11:58:46 +0300

    кнопка связаться с менеджером

--- external/scripts/vite.config.js
+++ external/scripts/vite.config.js
@@ -4,14 +4,13 @@ import monkey from 'vite-plugin-monkey';
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/miniPage.js',
+      entry: 'src/manager.js',
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
         match: [
           // 'https://www.coral.ru/hotels/turkey/*',
-          // 'https://www.coral.ru/booking/add-passenger*'
-            'https://www.sunmar.ru/preview/2c18a19f-1e77-4bad-9452-6c283d4c9543/ru-RU/'
+          'https://www.coral.ru/booking/add-passenger*'
         ],
       },
     }),
